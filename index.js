e4266930dcd9e2a899569cd0c670b80acb37b216
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-08-28 18:12:16 +0300

    completed assignments

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  //return tutorials.mao((tutorial)) => 
+
+	//uppercase first letter of each word in each tutorial
+	return tutorials.map((tutorial) => {
+		return tutorial
+			.split(" ")
+			.map((word) => {
+				return word[0].toUpperCase() + word.slice(1);
+			})
+			.join(" ");
+	});
+
+};
+
+
+
+  
+
+
